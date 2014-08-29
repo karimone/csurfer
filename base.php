9c05fdf551536c9cd266b45ac9edce112ab6af0e
Karim Norman Gorjux <karimone@gmail.com>
2014-08-29 18:23:56 +0300

    Updated the localization and added the SEO meta tags also for google checking

--- base.php
+++ base.php
@@ -14,10 +14,12 @@ require_once('inc/localization.php');
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>CamperSurfing</title>
+    <meta name="google-site-verification" content="RQXVur89NqSz21iNS0JlH1kTJK9M_52_XyPaQhuMOvI" />
+    <title><?php echo _('CamperSurfing: are you looking for great and safe camper or caravan spot?')?></title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="<?php echo _('Description of the page!'); ?>" name="description">
-    <meta content="CamperSurfing" name="author">
+    <meta name="description" content="<?php echo _('CamperSurfing helps Camper fans who are willing to find new places with people that can host them in their private parking space'); ?>">
+    <meta name="robots" content="NOODP">
+    <meta name="author" content="Karim Norman Gorjux">
 
     <!-- Le styles -->
     <link rel="stylesheet" href="css/bootstrap.css">
@@ -29,7 +31,6 @@ require_once('inc/localization.php');
     <!--[if lt IE 9]>
         <script src="js/html5shiv.js"></script>
     <![endif]-->
-
 
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
