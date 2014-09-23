63d60858e9d581b1e9753257cb9abb186bd652e1
Karim Norman Gorjux <karimone@gmail.com>
2014-09-23 15:11:37 +0300

    Added the small tablet version

--- idea.php
+++ idea.php
@@ -12,23 +12,23 @@
 <div class="container">
     <div id="idea" class="row">
     <div class="row">
-        <div class="col-lg-3 col-lg-offset-1 col-md-3 col-md-offset-1">
+        <div class="col-lg-3 col-lg-offset-1 col-md-3 col-md-offset-1 col-sm-3 col-sm-offset-1">
             <h2><?php echo _('The Idea'); ?></h2>
             <p><?php echo _('Who travels by camper has already experienced the sense of freedom and joy that one could feel only in free camping.'); ?></p>
         </div>
-        <div class="col-lg-5 col-lg-push-2 col-md-6 col-md-push-2">
+        <div class="col-lg-5 col-lg-push-2 col-md-6 col-md-push-2 col-sm-7 col-sm-offset-1">
             <img src="img/tablet-app.png" alt="Image of the campersurfer's app running on a tablet" class="img-responsive"/>
         </div>
     </div>
 
     <div class="row">
-        <div class="col-lg-5 col-lg-offset-1 col-md-5 col-md-offset-1">
+        <div class="col-lg-5 col-lg-offset-1 col-md-5 col-md-offset-1 col-sm-5 col-sm-offset-1">
             <p><?php echo _('Any camper traveller knows pretty well that it is not that easy to find out any places in campings, especially during high season period. '); ?>
             <?php echo _('It is possible to stay in equipped areas, but it might be dangerous  to get far from the van or even to leave it unattended for a while.'); ?></p>
             <p><?php echo _('To discover new paths, to visit barely known places or far from the most common  touristic seats is amazing, but sometimes it might be a tough task to combine "free destination" and "free stops" with a"safe parking".'); ?></p>
 
         </div>
-        <div class="col-lg-5 col-md-5">
+        <div class="col-lg-5 col-md-5 col-sm-5">
             <p><?php echo _('This is the reason why we are developing "CamperSurfing", a platform for  passionate free camper drivers who want to meet people available to host them in their own parking and to help them in discovering the treasures of their secret  places. Do you owe a private parking lot and are you able to host a camper van for one or more days?'); ?></p>
             <p><?php echo _('Do you usually travel by camper and you wish to discover new places and meet new people?'); ?></p>
             <p><?php echo _('Do you like all this? Come and visit us on Facebook and help us to improve CamperSurfing to fit your needs!'); ?></p>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <div class="container">
 <header class="row">
     <div class="row">
-        <div id="languages" class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4">
+        <div id="languages" class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-5 col-sm-offset-4">
             <!-- languages -->
             <img src="img/uk_flag.png" alt="English Flag Language" class="selected"/>
             <img src="img/it_flag.png" alt="Italian Flag Language"/>
@@ -16,7 +16,7 @@
             ?>
         </div>
 
-        <div id="social-links" class="col-lg-1 col-lg-offset-3 col-md-2 col-md-offset-2">
+        <div id="social-links" class="col-lg-1 col-lg-offset-3 col-md-2 col-md-offset-2 col-sm-2 col-sm-offset-1">
             <a class="" href="<?= FACEBOOK_LINK ?>">
                 <img src="img/facebook_icon.jpg" alt="Facebook Icon" class="img-rounded"
                      title="<?php echo _("Visit our Facebook Page") ?>"/>
@@ -29,10 +29,10 @@
     </div>
 
     <div class="row">
-        <div id="logo" class="col-lg-4 col-md-3">
+        <div id="logo" class="col-lg-4 col-md-4 col-sm-3">
             <img src="img/logo.png" alt="CamperSurfing Logo" class="img-responsive">
         </div>
-        <nav class="col-lg-8 col-md-8">
+        <nav class="col-lg-8 col-md-8 col-sm-9">
             <ul id="header-links">
                 <li><a id="home-link" href="#header" class="active">Home</a></li>
                 <li><a id="idea-link" href="#theidea"><?php echo _('The Idea'); ?></a></li>
@@ -46,7 +46,7 @@
 
 <section>
     <div id="banner" class="row">
-        <div id="infobox" class="col-lg-5 col-md-5">
+        <div id="infobox" class="col-lg-5 col-md-5 col-sm-7">
             <h1><?php echo _('Are you looking for great and safe camper spot?'); ?></h1>
             <h3><?php echo _('CamperSurfing let you find a spot for your camper in all the World!'); ?></h3>
             <form action="//gorjux.us1.list-manage.com/subscribe/post?u=7db505061fc2254f5c1957d0b&amp;id=f40b7187f5"
